fca8b918749a7db0881153b7cc0fb9f066b719ca
RobQuistNL <rob@dukesoft.nl>
2014-01-22 19:39:45 +0100

    Tabs

--- manifests/mailbox.pp
+++ manifests/mailbox.pp
@@ -1,9 +1,9 @@
 # A generic mailbox for 1 user.
 #
 
-class zarafa::mailbox ( #Name = email address
+class zarafa::mailbox (       #Name = email address
   $username         = undef,  #Will most likely be the same as the e-mail address. Defaults to $emailAddress
-  $fullName             = undef,  #A general readable name. Defaults to $emailAddress
+  $fullName         = undef,  #A general readable name. Defaults to $emailAddress
   $password,                  #Password for the account
 ) {
   $zadmin = $zarafa::params::zarafaAdminLocation
