7e60328b43aac1da34053bce487e10868ab257be
RobQuistNL <rob@dukesoft.nl>
2014-01-22 19:45:18 +0100

    fix params perhaps

--- manifests/mailbox.pp
+++ manifests/mailbox.pp
@@ -1,7 +1,7 @@
 # A generic mailbox for 1 user.
 #
 
-class zarafa::mailbox (       #Name = email address
+define zarafa::mailbox (       #Name = email address
   $username         = undef,  #Will most likely be the same as the e-mail address. Defaults to $emailAddress
   $fullName         = undef,  #A general readable name. Defaults to $emailAddress
   $password,                  #Password for the account
@@ -10,11 +10,15 @@ class zarafa::mailbox (       #Name = email address
   $emailAddress = $name
   
   if (undef == $username) {
-    $username = $emailAddress
+    $realUsername = $emailAddress
+  } else {
+    $realUsername = $username
   }
   
   if (undef == $fullName) {
-    $fullName = $emailAddress
+    $realFullName = $emailAddress
+  } else {
+    $realFullName = $fullName
   }
 
   exec{ "${zadmin} -c '${username}' -p '${password}' -e '${emailAddress}' -f '${fullName}'": } #@todo onlyif zarafa-admin --details {user} != Unable to resolve user
