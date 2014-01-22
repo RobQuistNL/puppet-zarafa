35c49ed669c2c23be34c338f8c9466b75941828e
RobQuistNL <rob@dukesoft.nl>
2014-01-22 18:46:11 +0100

    Fix mailbox class

--- manifests/mailbox.pp
+++ manifests/mailbox.pp
@@ -1,14 +1,22 @@
 # A generic mailbox for 1 user.
 #
 
-class zarafa::mailbox (
-  $emailAddress,              #Fully written out
+class zarafa::mailbox ( #Name = email address
   $username         = undef,  #Will most likely be the same as the e-mail address. Defaults to $emailAddress
-  $name             = undef,  #A general readable name. Defaults to $emailAddress
+  $fullName             = undef,  #A general readable name. Defaults to $emailAddress
   $password,                  #Password for the account
 ) {
   $zadmin = $zarafa::params::zarafaAdminLocation
+  $emailAddress = $name
+  
+  if (undef == $username) {
+    $username = $emailAddress
+  }
+  
+  if (undef == $fullName) {
+    $fullName = $emailAddress
+  }
 
-  exec{ "${zadmin} -c '${username}' -p '${password}' -e '${emailAddress}' -f '${name}'": } #@todo onlyif zarafa-admin --details {user} != Unable to resolve user
+  exec{ "${zadmin} -c '${username}' -p '${password}' -e '${emailAddress}' -f '${fullName}'": } #@todo onlyif zarafa-admin --details {user} != Unable to resolve user
 
 }
